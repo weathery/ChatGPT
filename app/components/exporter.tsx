0b3c06a0d7bd0d62bc2dd4c93f6cffd1c4f30ab6
weathery <weathery@qq.com>
2023-06-13 17:29:31 +0800

    rename the app name and desc.

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -437,9 +437,9 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>ChatGPT Next Web</div>
+            <div className={styles["main-title"]}>ChatGPT Assistant</div>
             <div className={styles["sub-title"]}>
-              github.com/Yidadaa/ChatGPT-Next-Web
+            Your private AI assistant.
             </div>
             <div className={styles["icons"]}>
               <ExportAvatar avatar={config.avatar} />
