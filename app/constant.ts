20d14511329ab5a7d73c3803e727bae39ab58cd8
weathery <weathery@qq.com>
2023-11-09 16:37:44 +0800

    Fix: fix the domain name

--- app/constant.ts
+++ app/constant.ts
@@ -7,10 +7,10 @@ export const RELEASE_URL = `${REPO_URL}/releases`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
-
-export const DEFAULT_CORS_HOST = "https://ab.nextweb.fun";
+export const DEFAULT_CORS_HOST = "https://chat.cnuco.org";
 export const DEFAULT_API_HOST = `${DEFAULT_CORS_HOST}/api/proxy`;
 export const OPENAI_BASE_URL = "https://api.openai.com";
+export const DEFAULT_API_HOST = "https://chat.cnuco.org/api/proxy";
 
 export enum Path {
   Home = "/",
